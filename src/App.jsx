3e1cec4fbf305c71700060c733e99e8c649e52de
kimoanh992812 <kimoanh992812@gmail.com>
2021-10-08 14:52:53 +0700

    build sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './component/Banner/Banner';
 import Header from './component/Header/Header';
+import Layout from "../src/layout/Layout"
 
 function App() {
     return (
         <>
             <Header/>
             <Banner/>
+            <Layout/>
         </>
     )
 }
